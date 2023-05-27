eaece1c80687e7377fa729457bb8bd8a4ae2822a
Shreyash Wadmalwar <sdwadmalwar@gmail.com>
2023-05-27 21:28:57 +0530

    Adding 5th module code to git repo

---
+++ practice/first_Step/hello_world.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Hello world</title>
+</head>
+
+<body>
+
+    <?php echo "Hello World !"; ?><br>
+    <?php print "Hello World !"; ?> <br>
+    <?php print "Hello" . " World !"; ?> <br>
+    <?php print 3 + 4; ?>
+</body>
+
+</html>
